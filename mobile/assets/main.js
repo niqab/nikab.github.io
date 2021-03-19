1b83c362d74c91de00f9b4a72c50cd9b31f0bb14
niqab <80803889+niqab@users.noreply.github.com>
2021-03-19 08:57:54 +0700

    Update main.js

--- mobile/assets/main.js
+++ mobile/assets/main.js
@@ -2,4 +2,8 @@
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });  
